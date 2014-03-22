57b927733e2b988979ea8c8e3f39ed6419480d1f
ios4341 <ios4341@caelum57-07.local>
2014-03-22 11:41:34 -0300

    Initial Commit

---
+++ HelloWorld2/MTKAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MTKAppDelegate.h
+//  HelloWorld2
+//
+//  Created by ios4341 on 22/03/14.
+//  Copyright (c) 2014 ios4341. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MTKViewController;
+
+@interface MTKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MTKViewController *viewController;
+
+@end
---
+++ HelloWorld2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld2
+//
+//  Created by ios4341 on 22/03/14.
+//  Copyright (c) 2014 ios4341. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTKAppDelegate class]));
+    }
+}
